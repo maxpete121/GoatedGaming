2113dc562dcd62adff4764cf21f248e1c4110550
Kaden <kjrigsbee@gmail.com>
2023-12-22 10:10:22 -0700

    huh??

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'dev-h6snzm7nk312z1nb.us.auth0.com'
+export const audience = 'http://sandyBoii.com'
+export const clientId = '1maRANVotkPMLKCvkkeYwWuz1BXzTlwh'
