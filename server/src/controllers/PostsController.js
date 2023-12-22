1663cea0f3857fdd3734cb173c5e92383968788d
maxpete121 <maxpete121@gmail.com>
2023-12-22 10:41:55 -0700

    Post up

--- server/src/controllers/PostsController.js
+++ server/src/controllers/PostsController.js
@@ -6,14 +6,34 @@ export class PostsController extends BaseController {
     constructor() {
         super('api/posts')
         this.router
-            .post('', this.createPost)
+        .get('', this.getPost)
+        .post('', this.createPost)
+        .delete('/:postId', this.deletePost)
     }
-    createPost(request, response, next) {
+    async createPost(request, response, next) {
         try {
             const postData = request.body
-            const userInfo = request.userInfo
+            // const userInfo = request.userInfo
             const post = await postsService.createPost(postData)
+            response.send(post)
+        } catch (error) {
+            next(error)
+        }
+    }
 
+    async getPost(request, response, next){
+     try {
+        await postsService.createPost()
+     } catch (error) {
+        next(error)
+     }   
+    }
+
+    async deletePost(request, response, next){
+        try {
+            const postId = request.params.postId
+            const deletedPost = await postsService.deletePost(postId)
+            return deletedPost
         } catch (error) {
             next(error)
         }
--- server/src/services/PostsService.js
+++ server/src/services/PostsService.js
@@ -1,8 +1,21 @@
+import { dbContext } from "../db/DbContext"
 
 
 class PostsService {
-    createPost(postData) {
-        const = await
+    async createPost(postData) {
+        const newPost = await dbContext.Posts.create(postData)
+        return newPost
+    }
+
+    async getPost(){
+        const allPost = await dbContext.Posts.find()
+        return allPost
+    }
+
+    async deletePost(postId){
+        const foundPost = await dbContext.Posts.findById(postId)
+        await foundPost.remove()
+        return `Post Deleted`
     }
 
 }
